43af34e4c026c6ac496e2ce02e9fc937ff681270
Juan Bautista Malina <malinajuanbautista@gmail.com>
2024-07-15 10:29:32 -0300

    Se añadieron proyectos con sus links de repositorio. También se hizo a la web responsiva. Se quitaron las ventanas modal.

---
+++ src/assets/js/btn-repository.js
@@ -0,0 +1,29 @@
+// Función para redirigir a los repositorios de GitHub
+function openGitHubRepo(projectName) {
+    let repoUrls = {
+        "To Do List": "https://github.com/juanbautistamalina/to-do-list",
+        "Tarjetas Añadibles": "https://github.com/juanbautistamalina/card-builder",
+        "Landing Page Running": "https://github.com/juanbautistamalina/freeCodeCamp-responsive-web-design-projects/tree/main/Landing%20Page",
+        "Web de Gimnasio": "https://github.com/juanbautistamalina/Gimnasio-Forte",
+        "Web de Homenaje": "https://github.com/juanbautistamalina/freeCodeCamp-responsive-web-design-projects/tree/main/Homenaje",
+        "Encuesta de Desarrollo Personal": "https://github.com/juanbautistamalina/freeCodeCamp-responsive-web-design-projects/tree/main/Encuesta"
+    };
+
+    if (repoUrls.hasOwnProperty(projectName)) {
+        window.open(repoUrls[projectName], "_blank"); // Abre en una nueva pestaña
+    } else {
+        alert("El repositorio para este proyecto no está disponible."); // Mensaje por si no se encuentra el proyecto
+    }
+}
+
+// Obtener todos los botones de proyecto y agregar el evento click
+document.addEventListener("DOMContentLoaded", function() {
+    let projectButtons = document.querySelectorAll('#portfolio button');
+    
+    projectButtons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            let projectName = button.querySelector("h4").innerText;
+            openGitHubRepo(projectName);
+        });
+    });
+});
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./assets/js/main.js";
 import "./assets/js/fixed.js";
+import "./assets/js/btn-repository.js";
 import "eins-modal/dist/js/eins-modal.min.js";
 import "./assets/css/index.css"
